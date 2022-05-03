55c8f540aa3150d9e2bd20fb12deef8fde5869cc
Rachel Marron <racheldmarron@gmail.com>
2022-05-03 15:10:36 -0700

    code for database and install js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database - PUT, DONE
+export const putDb = async (content) => {
+  console.log('PUTting data into the database');
+  const jateData = await openDB('jate', 1);
+  const tx = jateData.transaction('jate', 'readwrite');
+  const objectStr = tx.objectStore('jate');
+  const req = objectStr.put({ id: 1, value: content });
+  const result = await req;
+  console.log('Data successfully saved to the database!', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database - GET, DONE 
+export const getDb = async () => {
+console.log('GETting data from the database');
+const jateData = await openDB('jate', 1);
+const tx = jateData.transaction('jate', 'readonly');
+const objectStr = tx.objectStore('jate');
+const req = objectStr.get(1);
+const result = await req; 
+console.log('Data successfully saved to the database', result); 
+};
+
+// console.error('getDb not implemented');
 
 initdb();
